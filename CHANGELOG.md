f0d4224f8f87c03c3b77d688a5a5ec499eab6229
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2023-12-28 15:09:35 +0530

    v0.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,4 @@
-# 0.2.0 (28-12-2023)
+# 0.2.2 (28-12-2023)
+- **fixed:** improved `types` of `retry` function parameters.
+## 0.2.0 (28-12-2023)
 - **added:** `onSuccess` parameter in `retry` function. See [usage of retry](https://www.npmjs.com/package/utility-kit#utility)
--- src/utility.ts
+++ src/utility.ts
@@ -1,12 +1,11 @@
-type RetryOptions = { onSuccess?: Function | null, retries?: number, showError?: boolean }
+type RetryOptions<T> = { onSuccess?: ((result: T) => any) | null, retries?: number, showError?: boolean }
 
-export async function retry(cb: Function, { onSuccess = null, retries = 3, showError = false }: RetryOptions = {}) {
+export async function retry<T>(cb: () => T, { onSuccess = null, retries = 3, showError = false }: RetryOptions<T> = {}) {
     try {
         const result = await cb()
         await onSuccess?.(result)
         return result
-    }
-    catch (e) {
+    } catch (e) {
         if (showError) console.log(e);
         if (retries) return await retry(cb, { onSuccess, retries: retries - 1, showError })
     }
