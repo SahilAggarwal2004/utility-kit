4c9209bfdad8e56195d345c74716cc84b7f133d7
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2023-12-29 14:25:18 +0530

    v0.3.1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { maximumNumber, minimumNumber, probability } from "./math"
 import { generateOTP, randomAdjective, randomAnimal, randomElement, randomName, randomNumber } from "./random"
 import { wait } from "./time"
-import { retry } from "./utility"
-export { generateOTP, maximumNumber, minimumNumber, probability, randomAdjective, randomAnimal, randomElement, randomName, randomNumber, retry, wait }
+import { retry, retryAsync } from "./utility"
+export { generateOTP, maximumNumber, minimumNumber, probability, randomAdjective, randomAnimal, randomElement, randomName, randomNumber, retry, retryAsync, wait }
